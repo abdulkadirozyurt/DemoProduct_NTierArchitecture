4d569a3f829de50f0c16ac86ac78707b2c7bc3f7
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-01 15:26:56 +0300

    Business Layer Services added

---
+++ Business/Abstracts/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IGenericService<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+
+
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+
+    }
+}
