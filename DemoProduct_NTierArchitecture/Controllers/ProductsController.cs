f448abda22e734f8b66936829a634cc487728604
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-02 15:17:32 +0300

    Products Listed

---
+++ DemoProduct_NTierArchitecture/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Concretes;
+using DataAccess.EntityFramework;
+
+namespace DemoProduct_NTierArchitecture.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        public IActionResult Index()
+        {
+            var products = productManager.TGetAll();
+
+            return View(products);
+        }
+    }
+}
