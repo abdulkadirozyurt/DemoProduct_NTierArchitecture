5ef0d7c8999891ea6d957715e116aa6bad8e19c0
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-11 03:23:06 +0300

    .

--- DataAccess/Migrations/20230811001706_mig_add_AppUserandAppRole.Designer.cs
+++ DataAccess/Migrations/20230811001706_mig_add_AppUserandAppRole.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Concretes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(NTierArchitectureContext))]
-    partial class NTierArchitectureContextModelSnapshot : ModelSnapshot
+    [Migration("20230811001706_mig_add_AppUserandAppRole")]
+    partial class mig_add_AppUserandAppRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
